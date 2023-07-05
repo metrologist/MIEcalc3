Bug fix for covariance matrix issues. MIEcalculator.py split into MIEcalc3.py and reporting.py

formeqns.py
@@ -48,6 +48,7 @@ class EqnForm(IOForm):
             dof = len(X) - len(p)
             chisq = sum(info["fvec"] * info["fvec"])
             red_chisq = chisq / dof
+            cov = cov * red_chisq  # this is a July 2023 change
             fit_check = self.model.fit_qual(b_var, red_chisq, dof)
             ub0 = fit_check[0]
             temp_b0 = gtc.ureal(0, ub0)
@@ -61,7 +62,8 @@ class EqnForm(IOForm):
             label = []  # labels are required for fit_ureals
             for coefficient in p:
                 label.append(coefficient.name)
-            ucoeffs = self.model.fit_ureals(p2, cov * red_chisq, dof, label)  # note use of red_chisq
+            # ucoeffs = self.model.fit_ureals(p2, cov * red_chisq, dof, label)  # note use of red_chisq
+            ucoeffs = self.model.fit_ureals(p2, cov, dof, label)  # note red_chisq now done above, July 2023
             for_plotting = function.fn(XX, ucoeffs)
             Y = np.zeros(len(for_plotting))
             U = np.zeros(len(for_plotting))
@@ -100,6 +102,7 @@ class EqnForm(IOForm):
             dof = len(XY) - len(p)
             chisq = sum(info["fvec"] * info["fvec"])
             red_chisq = chisq / dof
+            cov = cov * red_chisq  # July 2023 modification
             fit_check = self.model.fit_qual(b_var, red_chisq, dof)
             ub0 = fit_check[0]
             temp_b0 = gtc.ureal(0, ub0)
@@ -126,7 +129,8 @@ class EqnForm(IOForm):
             label = []  # labels are required for fit_ureals
             for coefficient in p:
                 label.append(coefficient.name)
-            ucoeffs = self.model.fit_ureals(p2, cov * red_chisq, dof, label)  # note use of red_chisq
+            # ucoeffs = self.model.fit_ureals(p2, cov * red_chisq, dof, label)  # note use of red_chisq
+            ucoeffs = self.model.fit_ureals(p2, cov, dof, label)  # note red_chisq done above July 2023
             for_plotting = function.fn(RS, ucoeffs)
             ZZ = np.zeros(len(RS))
             UU = np.zeros(len(RS))

functions.py
@@ -149,8 +149,8 @@ class MODEL(object):
         Levenberg-Marquardt optimisation routine that returns  statistics
         of the least-squares fit and the fitted values of the coefficients.
         Input is in the form of a list of *data* values for independent
-        varialble *x* and a list of their uncertainties (for weighting) *u*.
-        The 'coefficents' are of PARAMETER class.
+        variable *x* and a list of their uncertainties (for weighting) *u*.
+        The 'coefficients' are of PARAMETER class.
         """
 
         def fitfun(params):
@@ -175,6 +175,7 @@ class MODEL(object):
         errors set to zero (e.g. no VT) or the input data has been fabricated to
         be a perfect fit.
         """
+        # print('MODEL fit_ureals ', cov)  # for checking correct covariance being used
         uncert = []
         for i in range(len(coeffs)):
             uncert.append(np.sqrt(cov[i, i]))